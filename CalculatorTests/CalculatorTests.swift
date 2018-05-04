fe325d03127c91944b71ad4bc067429c658002fd
rudeigerc <rudeigerc@gmail.com>
2018-05-05 01:59:18 +0800

    add some test cases

--- CalculatorTests/CalculatorTests.swift
+++ CalculatorTests/CalculatorTests.swift
@@ -6,36 +6,96 @@
 //  Copyright © 2018年 Rudeiger Cheng. All rights reserved.
 //
 
-import XCTest
 import Quick
 import Nimble
 @testable import Calculator
 
-class CalculatorTests: XCTestCase {
+class CalculatorTests: QuickSpec {
     
-    var calculatorViewController: CalculatorViewController!
-    
-    override func setUp() {
-        super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-        calculatorViewController = UIStoryboard(name: "Main", bundle: nil).instantiateInitialViewController() as! CalculatorViewController
-    }
-    
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-        calculatorViewController = nil
-        super.tearDown()
-    }
-    
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-    
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+    override func spec() {
+        
+        var calculatorViewController: CalculatorViewController!
+        
+        describe("calculator") {
+            
+            var button: UIButton!
+            
+            beforeEach {
+                calculatorViewController = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "CalculatorViewController") as! CalculatorViewController
+                _ = calculatorViewController.view
+            }
+            
+            describe("click button") {
+                beforeEach {
+                    button = UIButton()
+                }
+                
+                describe("when the button is a dot") {
+                    beforeEach {
+                        button.setTitle(".", for: .normal)
+                        calculatorViewController.touchDigit(button)
+                    }
+                    
+                    it("prints zero plus a dot") {
+                        
+                        expect(calculatorViewController.display.text).to(equal("0."))
+                    }
+                }
+                
+                describe("when the button is a digit") {
+                    beforeEach {
+                        button.setTitle("1", for: .normal)
+                        calculatorViewController.touchDigit(button)
+                    }
+                    
+                    it("prints the digit") {
+                        expect(calculatorViewController.display.text).to(equal("1"))
+                    }
+                    
+                    describe("when the button is a dot") {
+                        beforeEach {
+                            button.setTitle(".", for: .normal)
+                            calculatorViewController.touchDigit(button)
+                        }
+                        
+                        it("prints one plus a dot") {
+                            expect(calculatorViewController.display.text).to(equal("1."))
+                        }
+                    }
+                    
+                    describe("when the button is a digit") {
+                        beforeEach {
+                            button.setTitle("2", for: .normal)
+                            calculatorViewController.touchDigit(button)
+                        }
+                        
+                        it("prints one plus a dot") {
+                            expect(calculatorViewController.display.text).to(equal("12"))
+                        }
+                    }
+                }
+                
+                describe("when the button is C") {
+                    beforeEach {
+                        calculatorViewController.resetAll(button)
+                    }
+                    
+                    it("clears all") {
+                        expect(calculatorViewController.display.text).to(equal("0"))
+                        expect(calculatorViewController.userIsInTheMiddleOfTyping).to(equal(false))
+                        expect(calculatorViewController.brain.accumulator).to(equal(0.0))
+                        expect(calculatorViewController.brain.description.isEmpty).to(equal(true))
+                    }
+                }
+                
+                afterEach {
+                    button = nil
+                }
+            }
+            
+            afterEach {
+                calculatorViewController = nil
+            }
         }
     }
     
